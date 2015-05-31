e2ca51f2764f31295de50be3cdc5cf7ebce97dc3
Ubuntu 64 <amitbhatia76@gmail.com>
2015-05-31 09:13:43 -0700

    change some files

--- bst.c
+++ bst.c
@@ -9,7 +9,8 @@ typedef struct binary_tree tnode;
 struct binary_tree {
     tnode *l;
     tnode *r;
-    int val;   
+    int val;  
+    int height; 
 };
 
 enum TREE_CHOICE {
@@ -185,7 +186,8 @@ int _print_t(tnode *tree, int is_left, int offset, int depth, char s[20][255])
 {
     char b[20];
     int width = 5;
-    
+    int i;
+
     if (!tree) return 0;
     
     sprintf(b, "(%03d)", tree->val);
@@ -194,30 +196,30 @@ int _print_t(tnode *tree, int is_left, int offset, int depth, char s[20][255])
     int right = _print_t(tree->r, 0, offset + left + width, depth + 1, s);
     
 #ifdef COMPACT
-    for (int i = 0; i < width; i++)
+    for (i = 0; i < width; i++)
         s[depth][offset + left + i] = b[i];
     
     if (depth && is_left) {
         
-        for (int i = 0; i < width + right; i++)
+        for (i = 0; i < width + right; i++)
             s[depth - 1][offset + left + width/2 + i] = '-';
         
         s[depth - 1][offset + left + width/2] = '.';
         
     } else if (depth && !is_left) {
         
-        for (int i = 0; i < left + width; i++)
+        for (i = 0; i < left + width; i++)
             s[depth - 1][offset - width/2 + i] = '-';
         
         s[depth - 1][offset + left + width/2] = '.';
     }
 #else
-    for (int i = 0; i < width; i++)
+    for (i = 0; i < width; i++)
         s[2 * depth][offset + left + i] = b[i];
     
     if (depth && is_left) {
         
-        for (int i = 0; i < width + right; i++)
+        for ( i = 0; i < width + right; i++)
             s[2 * depth - 1][offset + left + width/2 + i] = '-';
         
         s[2 * depth - 1][offset + left + width/2] = '+';
@@ -225,7 +227,7 @@ int _print_t(tnode *tree, int is_left, int offset, int depth, char s[20][255])
         
     } else if (depth && !is_left) {
         
-        for (int i = 0; i < left + width; i++)
+        for ( i = 0; i < left + width; i++)
             s[2 * depth - 1][offset - width/2 + i] = '-';
         
         s[2 * depth - 1][offset + left + width/2] = '+';
@@ -239,12 +241,14 @@ int _print_t(tnode *tree, int is_left, int offset, int depth, char s[20][255])
 int print_t(tnode *tree)
 {
     char s[20][255];
-    for (int i = 0; i < 20; i++)
+    int i;
+
+    for (i = 0; i < 20; i++)
         sprintf(s[i], "%80s", " ");
     
     _print_t(tree, 0, 0, 0, s);
     
-    for (int i = 0; i < 20; i++)
+    for (i = 0; i < 20; i++)
         printf("%s\n", s[i]);
     return 0;
 }
